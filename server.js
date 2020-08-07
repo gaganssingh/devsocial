88491cbb3e052c1b7e93728bf6c1ff4be6ba1e4f
Gagan Singh <gagansingh.dev@outlook.com>
2020-08-06 20:55:45 -0400

    created basic routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const usersRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
+const profileRouter = require("./routes/api/profile");
+const postsRouter = require("./routes/api/posts");
 
 const app = express();
 
@@ -7,6 +12,12 @@ const app = express();
 connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/posts", postsRouter);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () =>
